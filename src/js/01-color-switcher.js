bcb07bc3e788884e8f530be71add0ce5145188de
aprilespiritu <april_espiritu@live.com>
2024-04-29 00:11:07 +0800

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)
+        }`;
+}
+
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let colorChangeInterval = null;
+stopBtn.disabled = true;
+
+function startColorChange() {
+    colorChangeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        stopBtn.disabled = false;
+        startBtn.disabled = true;
+    }, 1000)
+}
+
+function stopColorChange() {
+    clearInterval(colorChangeInterval);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
